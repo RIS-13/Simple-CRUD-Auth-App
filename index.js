3c147ab7f6c9921a90570b73d2773ba1c04d43a3
Kassad <i@kassad.ru>
2016-02-25 18:35:32 +0500

    Добавлена структура Веб-сервера, а так же маршрутизация запросов

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var app = express();
+
+var staticRouter = require('./routes/static');
+var apiRouter = require('./routes/api');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", staticRouter);
+app.use("/api/", apiRouter);
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+});
---
+++ routes/api.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var Todo = require('../models/todo');
+
+// TODO: add real API methods
+router.route("methodName/:id")
+    .get(function (req, res) {
+        res.json({'method': 'get', 'id': req.params.id});
+    })
+    .put(function (req, res) {
+        res.json({'method': 'put', 'id': req.params.id});
+    })
+    .delete(function (req, res) {
+        res.json({'method': 'delete', 'id': req.params.id});
+    });
+
+router.route("*")
+    .all(function (req, res) {
+        res.json({'error': 'method not founded'});
+    });
+module.exports = router;

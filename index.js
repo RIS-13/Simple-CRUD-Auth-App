503beadac50fc7ab8f82945625bfbfca251cc756
Kassad <i@kassad.ru>
2016-03-04 23:40:08 +0500

    Добавлена статика

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var app = express();
 
-var staticRouter = require('./routes/static');
 var apiRouter = require('./routes/api');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", staticRouter);
 app.use("/api/", apiRouter);
 
 app.set('port', process.env.PORT || 3000);
